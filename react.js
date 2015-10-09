3e3e4bdd280f5a206a4e50fcfcb51fa1c1a2de74
angelozerr <angelo.zerr@gmail.com>
2015-10-09 22:46:42 +0200

    Customize acorn walk with JSX.

--- react.js
+++ react.js
@@ -1,15 +1,31 @@
 (function(mod) {
     if (typeof exports == "object" && typeof module == "object") // CommonJS
-        return mod(require("tern/lib/infer"), require("tern/lib/tern"), require("acorn/dist/acorn"), require("acorn-jsx"));
+        return mod(require("tern/lib/infer"), require("tern/lib/tern"), require("acorn/dist/acorn"), require("acorn/dist/walk"), require("acorn-jsx"));
     if (typeof define == "function" && define.amd) // AMD
-        return define([ "tern/lib/infer", "tern/lib/tern", "acorn/dist/acorn", "acorn-jsx" ], mod);
-    mod(tern, tern, acorn, acornJSX);
-})(function(infer, tern, acorn, acornJSX) {
+        return define([ "tern/lib/infer", "tern/lib/tern", "acorn/dist/acorn", "acorn/dist/walk", "acorn-jsx" ], mod);
+    mod(tern, tern, acorn, acorn.walk, acornJSX); // Plain browser env
+})(function(infer, tern, acorn, walk, acornJSX) {
   "use strict";
-    
+
+  // Override acorn.walk with JSX
+  // see https://github.com/chtefi/acorn-jsx-walk/blob/master/index.js
+  var base = walk.base;
+  base.JSXElement = function (node, st, c) {
+   // node.openingElement.name
+   node.children.forEach(function (n) {
+     c(n, st);
+   });
+   // node.closingElement.name
+  };
+
+  base.JSXExpressionContainer = function (node, st, c) {
+    c(node.expression, st);
+  };
+  
+  
   tern.registerPlugin("react", function(server, options) {
-	  acornJSX(acorn);
-	  server.on("preParse", preParse);	  
+    acornJSX(acorn);
+    server.on("preParse", preParse);
   });
   
   function preParse(text, options) {
